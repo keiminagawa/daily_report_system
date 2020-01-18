b272448eca0de9351179fb82d04739062fbd2be6
kei.minagawa <ekutoart3554@gmail.com>
2020-01-18 20:17:02 +0900

    Add Employee create

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -13,10 +13,10 @@ import javax.persistence.Table;
 
 @Table(name = "employees")
 @NamedQueries({
-		@NamedQuery(name = "getAllEmployees", query = "select e from Employee as e order by e.id desc"),
-		@NamedQuery(name = "getEmployeesCount", query = "select count(e) from Employee as e"),
-		@NamedQuery(name = "checkRegisteredCode", query = "select count(e) from Employee as e where e.code = :code"),
-		@NamedQuery(name = "checkLoginCodeAndPassword", query = "select e from Employee as e where e.delete_flag = 0 and  e.code = :code and e.password = :pass")
+		@NamedQuery(name = "getAllEmployees", query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"),
+		@NamedQuery(name = "getEmployeesCount", query = "SELECT COUNT(e) FROM Employee AS e"),
+		@NamedQuery(name = "checkRegisteredCode", query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"),
+		@NamedQuery(name = "checkLoginCodeAndPassword", query = "SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass")
 })
 @Entity
 public class Employee {
